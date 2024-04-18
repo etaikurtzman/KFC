d853b6aaa011f2e41534bd30f90db3a2ea7a299e
syoukeles <syoukeles@gmail.com>
2024-04-17 23:07:54 -0400

    Two players now works

--- player.py
+++ player.py
@@ -4,6 +4,21 @@ import threading
 
 BOARD_LENGTH = 800
 
+piece_images = {
+            'wb': pygame.transform.scale(pygame.image.load('imgs/w_bishop.png'), (100, 100)),
+            'bb': pygame.transform.scale(pygame.image.load('imgs/b_bishop.png'), (100, 100)),
+            'wk': pygame.transform.scale(pygame.image.load('imgs/w_king.png'), (100, 100)),
+            'bk': pygame.transform.scale(pygame.image.load('imgs/b_king.png'), (100, 100)),
+            'wn': pygame.transform.scale(pygame.image.load('imgs/w_knight.png'), (100, 100)),
+            'bn': pygame.transform.scale(pygame.image.load('imgs/b_knight.png'), (100, 100)),
+            'wp': pygame.transform.scale(pygame.image.load('imgs/w_pawn.png'), (100, 100)),
+            'bp': pygame.transform.scale(pygame.image.load('imgs/b_pawn.png'), (100, 100)),
+            'wq': pygame.transform.scale(pygame.image.load('imgs/w_queen.png'), (100, 100)),
+            'bq': pygame.transform.scale(pygame.image.load('imgs/b_queen.png'), (100, 100)),
+            'wr': pygame.transform.scale(pygame.image.load('imgs/w_rook.png'), (100, 100)),
+            'br': pygame.transform.scale(pygame.image.load('imgs/b_rook.png'), (100, 100))
+        }
+
 def main():
     serverIP = input("Enter the server's IPv4: ")
     player = Player(serverIP)
@@ -21,22 +36,25 @@ class Player:
         self.screen = pygame.display.set_mode([BOARD_LENGTH, BOARD_LENGTH])
         self.length = 8
         self.pixelLength = BOARD_LENGTH
-        # self.stop_event = threading.Event()
-
-    
 
     def getUpdatesLoop(self):
         while True:
-            board = self.network.receive()
+            msg = self.network.receive()
+            print("msg is: ", msg)
+            if msg == "Quit":
+                print("getUpdatesLoop has stopped")
+                break
+            board = msg
+            print("received more from player")
+            print(board)
             self.screen.fill('yellow')
             self.draw_board()
             self.draw_pieces(board)
             
             pygame.draw.circle(self.screen, (200, 0, 255), (BOARD_LENGTH - 250, BOARD_LENGTH - 250), 75)
-
+            
             pygame.display.update()
-    
-    
+
     def getMovesLoop(self):
         print("In getMovesLoop!")
         running = True
@@ -44,6 +62,7 @@ class Player:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                    self.network.sendMove("Quit")
                     # self.stop_event.set()
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -56,38 +75,23 @@ class Player:
                         mouse_x, mouse_y = event.pos
                         end = (mouse_x // (BOARD_LENGTH // 8), mouse_y // (BOARD_LENGTH // 8))
                         if start and end:
+                            print("Sending move to server")
                             self.network.sendMove(str((start, end)))
                         start = None
                         end = None
-            
+        
+        
         pygame.quit()
-
-
-    def get_image(self, piece_string):
-        piece_images = {
-            'wb': pygame.image.load('imgs/w_bishop.png'),
-            'bb': pygame.image.load('imgs/b_bishop.png'),
-            'wk': pygame.image.load('imgs/w_king.png'),
-            'bk': pygame.image.load('imgs/b_king.png'),
-            'wn': pygame.image.load('imgs/w_knight.png'),
-            'bn': pygame.image.load('imgs/b_knight.png'),
-            'wp': pygame.image.load('imgs/w_pawn.png'),
-            'bp': pygame.image.load('imgs/b_pawn.png'),
-            'wq': pygame.image.load('imgs/w_queen.png'),
-            'bq': pygame.image.load('imgs/b_queen.png'),
-            'wr': pygame.image.load('imgs/w_rook.png'),
-            'br': pygame.image.load('imgs/b_rook.png')
-        }
-        return piece_images.get(piece_string)
+        print("Get moves loop stopped")
  
     def draw_pieces(self, encoded_board):
+        print("draw pieces")
         decoded_board = encoded_board.split(',')
         index = 0
         for i in range(self.length):
             for j in range(self.length):
                 if decoded_board[index] != ".":
-                    piece_image = self.get_image(decoded_board[index])
-                    piece_image = pygame.transform.scale(piece_image, (100, 100))
+                    piece_image = piece_images.get(decoded_board[index])
                     self.screen.blit(piece_image, ((i * (self.pixelLength // self.length)), (j * (self.pixelLength // self.length))))
                 index += 1
 
@@ -99,8 +103,6 @@ class Player:
                 if ((i + j) % 2 == 1):
                     square_dim = BOARD_LENGTH / 8
                     pygame.draw.rect(self.screen, 'orange', (i * square_dim, j * square_dim, BOARD_LENGTH / 8, BOARD_LENGTH / 8))
-            
-
 
 if __name__ == '__main__':
     main()
--- servertest.py
+++ servertest.py
@@ -25,37 +25,37 @@ def main():
     threads = []
     colors = ['white', 'black']
     conns = []
-
     while numPlayers < MAX_USERS:
         conn, addr = s.accept()
         print("Connected to: ", addr)
+
+        #playerColor = colors[numPlayers]
+        # thread = threading.Thread(target=client_loop, args=[conn, playerColor, board])
+        # threads.append(thread)
+        # thread.start()
         conns.append(conn)
         numPlayers += 1
+        if numPlayers == MAX_USERS:
+            thread1 = threading.Thread(target=client_loop, args=[conns[0], conns[1], colors[0], board])
+            thread2 = threading.Thread(target=client_loop, args=[conns[1], conns[0], colors[1], board])
+            threads = [thread1, thread2]
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
 
-    thread1 = threading.Thread(target=client_loop, args=[conns[0], conns[1], colors[0], board])
-    thread2 = threading.Thread(target=client_loop, args=[conns[1], conns[0], colors[1], board])
-    threads = [thread1, thread2]
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
-    print("Server loop stopped")
+
+    # for thread in threads:
+    #     thread.join()
+    
+
 
 def client_loop(conn1, conn2, playerColor, board):
     conn1.send(str.encode(playerColor))
     conn1.sendall(str.encode(board.grid_to_string()))
     while True:
         try:
-            msg = conn1.recv(2048).decode()
-            print(type(msg))
-            print("server msg is: ", msg)
-            if msg == "Quit":
-                print("Quit has been recieved")
-                conn1.sendall(str.encode("Quit"))
-                # conn2.sendall(str.encode("Quit"))
-                print("Client loop has stopped")
-                break
-            start, end = eval(msg)
+            start, end = eval(conn1.recv(2048))
             board.move(start, end)
             conn1.sendall(str.encode(board.grid_to_string()))
             conn2.sendall(str.encode(board.grid_to_string()))
